ecf84339d763ff2e13891113aa60edcce711b882
BiwashGurung <varungrg10@gmail.com>
2023-08-18 17:09:53 +0545

    feat(API) : coded function and addedAPI in it

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ fileInp = form.querySelector("input"),
 infoText = form.querySelector("p"),
 closeBtn = document.querySelector(".close"),
 copyBtn = document.querySelector(".copy");
+
+function fetchRequest(file, formData) {
+    infoText.innerText = "Scanning QR Code...";
+    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+        method: 'POST', body: formData
+    }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
+        if(!result) return;
+        document.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
+        wrapper.classList.add("active");
+    }).catch(() => {
+        infoText.innerText = "Couldn't scan QR Code";
+    });
+}
