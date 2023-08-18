468028e56bc631e1dd0084a68473c7dec14f1126
BiwashGurung <varungrg10@gmail.com>
2023-08-18 17:08:39 +0545

    feat : added variable

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper"),
+form = document.querySelector("form"),
+fileInp = form.querySelector("input"),
+infoText = form.querySelector("p"),
+closeBtn = document.querySelector(".close"),
+copyBtn = document.querySelector(".copy");
